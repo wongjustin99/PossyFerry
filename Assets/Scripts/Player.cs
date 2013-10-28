4ae00b10a6da369fcd7e164ac5c5b87c39296611
unknown <kevinnieman@gmail.com>
2013-10-28 16:43:00 -0400

    Added shotcontainer global class functionality

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,4 +6,16 @@ public class Player : FSprite {
 	public Player() : base("Banana")
 	{
 	}
+	
+	public void playerDeath()
+	{
+		//return player to the middle of the screen
+		this.x = Futile.screen.halfWidth;
+		this.y = Futile.screen.halfHeight;
+		
+		Debug.Log (" you died");
+		
+		//handle any death consequences
+	}
+	
 }
--- Assets/Scripts/ShotContainer.cs
+++ Assets/Scripts/ShotContainer.cs
@@ -6,11 +6,13 @@ using System;
 public class ShotContainer :  FContainer
 {
 	private Shot _shot;
-	private List<Shot> _shots = new List<Shot>();
+	private static List<Shot> _allShots = new List<Shot>();
+	private static List<Shot> _enemyShots = new List<Shot>();
+	private static List<Shot> _playerShots = new List<Shot>();
+	public static ShotContainer theShots;
 	
 	public void Start()
 	{	
-		
 		
 	}
 	
@@ -18,6 +20,50 @@ public class ShotContainer :  FContainer
 	public void Update()
 	{
 		
+	}
+	
+	public static void addPlayerShotToContainer(Shot shot)
+	{
+		_playerShots.Add(shot);
+		_allShots.Add (shot);
+		Futile.stage.AddChild(shot);
+	}
+	
+	public static void addEnemyShotToContainer(Shot shot)
+	{
+		_enemyShots.Add(shot);
+		_allShots.Add (shot);
+		Futile.stage.AddChild(shot);
+	}
+	
+	
+	public static void deletePlayerShotFromContainer(Shot shot)
+	{
+		_playerShots.Remove (shot);
+		_allShots.Remove(shot);
+		shot.RemoveFromContainer();
+	}
+	
+	public static void deleteEnemyShotFromContainer(Shot shot)
+	{
+		_enemyShots.Remove(shot);
+		_allShots.Remove(shot);
+		shot.RemoveFromContainer();
+	}
+	
+	public List<Shot> requestAllShots()
+	{
+		return _allShots;
+	}
+	
+	public List<Shot> requestEnemyShots()
+	{
+		return _enemyShots;
+	}
+	
+	public List<Shot> requestPlayerShots()
+	{
+		return _playerShots;
 	}
 	
 }
