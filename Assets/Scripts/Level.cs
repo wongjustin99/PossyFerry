8916276586f438ae56574d71a7e089b0572b0975
Justin Wong <registered99@gmail.com>
2013-11-16 19:19:27 -0500

    temp save to bring code to laptop

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -1,10 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Level 
+public abstract class Level 
 {
 	protected FSprite _background;
     protected GamePage _gamepage;
+    protected Queue<LevelEvent> _eventQueue = new Queue();
+
     public Level(GamePage _gamepage)
     {
         // how about a level takes in a BG string?  -- Level manager (not written) would be able to iterate/dole out "levels"
@@ -12,9 +14,14 @@ public class Level
             // Level could be able to farm out to a text file, but "for now", how about just subclassing `Level` for each one? 
         this._gamepage = _gamepage;
     }
+
     protected void setBackground( string _background_file ){
         _background = new FSprite( _background_file );
         _gamepage.AddChild(_background);
     }
 
+    public Time nextEventTime()
+    {
+        return _eventQueue.Peek();
+    }
 }
---
+++ Assets/Scripts/LevelEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelEvent {
+    private Time _triggerTime;
+    private string _eventAction;
+
+    public Time getTime()
+    {
+        return _triggerTime;
+    }
+
+    public Time getEventAction()
+    {
+        return _eventAction;
+    }
+}
